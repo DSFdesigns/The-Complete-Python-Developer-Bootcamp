56dde49be2be506f5a37b5aea13467191b4570bd
Andre F <50321935+DSFdesigns@users.noreply.github.com>
2026-07-08 22:44:58 +0900

    20260708
    Updates to bootcamp

---
+++ The Complete Python Developer Course/65. Ternary Operator.py
@@ -0,0 +1,10 @@
+# 66. Ternary Operator or Conditional Expression (shortcut to if else)(for True or False conditions)
+
+# condition_if_true if condition else condition_if_else
+is_friend = False
+can_message = "message allowed" if is_friend else "not allowed to message"
+print(can_message)
+
+is_chubby = True
+on_diet = "cannot eat lots of food" if is_chubby else "can eat lots of food"
+print(on_diet)
