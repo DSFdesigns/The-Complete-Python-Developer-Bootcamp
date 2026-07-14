4197ddaa02663febc81f9bd440a078665ddf0d19
Andre F <50321935+DSFdesigns@users.noreply.github.com>
2026-07-14 22:43:22 +0900

    20260714
    Updates to bootcamp

---
+++ The Complete Python Developer Course/70. For Loops.py
@@ -0,0 +1,10 @@
+# 70. For Loops ('Zero to Mastery') is an itterable
+# for item in [1, 2, 3, 4, 5]:
+#    print(item)
+#   print(item)
+#print(item)
+# Nested items
+for item in (1, 2, 3, 4, 5):
+    for x in ['a', 'b', 'c']:
+        print(item, x)
+
