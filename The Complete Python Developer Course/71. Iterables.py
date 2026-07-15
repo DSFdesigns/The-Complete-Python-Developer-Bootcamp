913e397b894a105935073a894cdff707985ff243
Andre F <50321935+DSFdesigns@users.noreply.github.com>
2026-07-15 22:47:22 +0900

    20260715
    Updates to bootcamp

---
+++ The Complete Python Developer Course/71. Iterables.py
@@ -0,0 +1,20 @@
+# 71. Iterables (list, dictionary, tuple, set, string
+# Iterate = check one by one check each item in the collection
+#for item in (1, 2, 3, 4, 5):
+#  for x in ['a', 'b', 'c']:
+ #       print(1, 'c')
+
+user = {
+    'name': 'Golem',
+    'age': 5006,
+    'can_swim': False
+}
+
+for key, value in user.items():
+    print(key, value)
+
+for item in user.values():
+    print(item)
+
+for item in user.keys():
+    print(item)
