633087803f99fa5b375cbe50ef13429930afde5a
Andre F <50321935+DSFdesigns@users.noreply.github.com>
2026-07-19 15:49:27 +0900

    20260719
    Updates to python bootcamp

--- The Complete Python Developer Course/71. Iterables.py
+++ The Complete Python Developer Course/71. Iterables.py
@@ -10,11 +10,11 @@ user = {
     'can_swim': False
 }
 
-for key, value in user.items():
-    print(key, value)
+#for key, value in user.items():
+#    print(key, value)
 
 for item in user.values():
     print(item)
 
-for item in user.keys():
-    print(item)
+#for item in user.keys():
+#    print(item)
