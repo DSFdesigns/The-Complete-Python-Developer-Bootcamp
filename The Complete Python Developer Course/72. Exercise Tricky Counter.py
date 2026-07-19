ff49f059942e17d3d880f97ee3e2ba4c8fcda91f
Andre F <50321935+DSFdesigns@users.noreply.github.com>
2026-07-19 22:49:38 +0900

    20260719
    Updates to python bootcamp

--- The Complete Python Developer Course/72. Exercise Tricky Counter.py
+++ The Complete Python Developer Course/72. Exercise Tricky Counter.py
@@ -1,8 +1,11 @@
 # Counter (loop over the iterable list and sum of the total of the list)
 my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-sum = 0
-for x in my_list:
-	sum = sum + x 
-	print(sum)
+#sum = 0
+#for x in my_list:
+#	sum = sum + x 
+#	print(sum)
 
-
+counter = 0
+for item in my_list:
+	counter = counter + item 
+	print(counter)
---
+++ The Complete Python Developer Course/73. Range.py
@@ -0,0 +1,16 @@
+# 73. range()
+
+#for _ in range(0, 10):
+#	print(_)
+	
+#for _ in range(0, 10, 2):
+#	print(_)
+	
+#for _ in range(10, 0, -2): # Print in reverse
+#	print(_)
+
+for _ in range(2):
+	print(list(range(10)))
+	
+
+
