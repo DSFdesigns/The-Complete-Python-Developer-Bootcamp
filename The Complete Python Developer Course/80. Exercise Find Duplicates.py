ffc57c8e93ad2f433eee98600974fe3feb92f39d
Andre F <50321935+DSFdesigns@users.noreply.github.com>
2026-08-13 22:31:40 +0900

    20260813
    Updates to crashcourse

--- The Complete Python Developer Course/80. Exercise Find Duplicates.py
+++ The Complete Python Developer Course/80. Exercise Find Duplicates.py
@@ -4,8 +4,25 @@ duplicates = []
 
 # print the duplicate values in a list
 
-for values in some_list:
-	if some_list.count(values) > 1:
-		if values not in duplicates:
-			duplicates.append(values)
-print("Duplicate Values are: ", duplicates)
+#for values in some_list:
+#	if some_list.count(values) > 1:
+#		if values not in duplicates:
+#			duplicates.append(values)
+#print("Duplicate Values are: ", duplicates)
+
+# Instructor solution
+for value in some_list:
+	if some_list.count(value) > 1: # if value is more than one
+		if value not in duplicates:
+			duplicates.append(value)
+print(duplicates)
+
+
+cat_list = ['debu', 'young', 'gato', 'gato', 'kan chan']
+dupes = []
+
+for cats in cat_list:
+	if cat_list.count(cats) > 1:
+		if cats not in dupes:
+			dupes.append(cats)
+print(dupes)
