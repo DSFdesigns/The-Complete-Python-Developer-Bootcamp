f6f417ca30e8c95e551fd26ddab2cf36e85c8353
Andre F <50321935+DSFdesigns@users.noreply.github.com>
2026-08-26 22:49:21 +0900

    Updates to bootcamp

--- The Complete Python Developer Course/85. Exercise Tesla.py
+++ The Complete Python Developer Course/85. Exercise Tesla.py
@@ -9,12 +9,12 @@
 # 1 (done)
  def checkDriverAge():
 	age = input("What is your age?: ")
-	if int(age) < 18:
-		print("Sorry, you are too young to drive this car. Powering off")
- 	elif int(age) > 18:
- 		print("Powering On. Enjoy the ride!");
- 	elif int(age) == 18:
-		print("Congratulations on your first year of driving. Enjoy the ride!")
+		if int(age) < 18:
+			print("Sorry, you are too young to drive this car. Powering off")
+		elif int(age) > 18:
+			print("Powering On. Enjoy the ride!");
+		elif int(age) == 18:
+			print("Congratulations on your first year of driving. Enjoy the ride!")
 checkDriverAge()
 
 # 2 (Done)
