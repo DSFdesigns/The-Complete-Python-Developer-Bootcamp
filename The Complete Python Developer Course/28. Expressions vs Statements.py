3483bfadfd9a567d2557caba708c85b2c98a2308
Andre F <50321935+DSFdesigns@users.noreply.github.com>
2026-09-02 21:38:24 +0900

    20260902
    Updates to bootcamp

--- The Complete Python Developer Course/28. Expressions vs Statements.py
+++ The Complete Python Developer Course/28. Expressions vs Statements.py
@@ -1,5 +1,7 @@
 # expression (produces a value) as iq / 5
 # statements (performs an action) user_age = iq / 5
 # iq = 100 is also a statement
-iq = 100
-user_age = iq / 5
+iq = 100 # This is a statement
+user_age = iq / 5 # This is an expression
+
+print(user_age)
