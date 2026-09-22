229576ced15f7c4f62a7e31423438138e39de3b9
Andre F <50321935+DSFdesigns@users.noreply.github.com>
2026-09-22 22:04:16 +0900

    20260916 Updates to bootcamp

---
+++ 95. nonlocal keyword.py
@@ -0,0 +1,16 @@
+# 95. Nonlocal keyword (refers to parent local)
+def outer():
+    x = "local"
+    def inner():
+        nonlocal x
+        x = "nonlocal"
+        print("inner:", x)
+
+    inner()
+    print("outer:", x)
+
+outer()
+# 1 - start with local
+# 2 - Parent local?
+# 3 - Global
+# Used in closures
