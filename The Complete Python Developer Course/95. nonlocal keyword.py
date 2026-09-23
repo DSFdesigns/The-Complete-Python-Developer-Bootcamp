0ccc411fde12f2a75633dd2cdd0799ff9dd36d1c
Andre F <50321935+DSFdesigns@users.noreply.github.com>
2026-09-23 21:07:07 +0900

    20260923
    Updates to bootcamp

--- The Complete Python Developer Course/95. nonlocal keyword.py
+++ The Complete Python Developer Course/95. nonlocal keyword.py
@@ -1,18 +1,30 @@
 # 95. Nonlocal keyword (refers to parent local)
-def outer():
-    x = "local"
-    def inner():
-        nonlocal x
-        x = "nonlocal"
-        print("inner:", x)
+#def outer():
+#    x = "local"
+#    def inner():
+#        nonlocal x
+#        x = "nonlocal"
+#        print("inner:", x)
 
-    inner()
-    print("outer:", x)
+#   inner()
+#    print("outer:", x)
 
-outer()
+#outer()
+
 # 1 - start with local
 # 2 - Parent local?
 # 3 - Global
 # Used in closures
 
 # Practice area:
+def debu():
+	d = "boss"
+	def young():
+		nonlocal d
+		d = "nonlocal"
+		print("young:", d)
+		
+	young()
+	print("debu", d)
+		
+debu()
